702d023e59c3cb48fa49b63efa20193c6b0b6546
Navaneeth Gade <navaneethgade07@gmail.com>
2024-07-02 23:55:29 +0530

    Test : authController(update)

--- test/authController.test.js
+++ test/authController.test.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authRoutes = require("../routes/authRoutes");
-const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require("../middlewares/authMiddleware");
 const User = require("../models/userModel");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json());
@@ -16,14 +16,10 @@ app.use("/api", authRoutes);
 // mock the userModel
 jest.mock("../models/userModel");
 
-
 // mock the authMiddleware
-jest.mock("../middlewares/authMiddleware" , ()=> {
-  jest.fn((req,res,next)=>{
-    next();
-  })
-});
-
+jest.mock("../middlewares/authMiddleware", () =>
+  jest.fn((req, res, next) => next())
+);
 
 describe("Auth controller", (done) => {
   let server;
@@ -39,7 +35,6 @@ describe("Auth controller", (done) => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-
 
   // Test for Register functonality
   it("should a register a new user", async () => {
@@ -62,7 +57,6 @@ describe("Auth controller", (done) => {
     expect(response.status).toBe(201);
     expect(response.body.message).toBe("User Successfully Registered");
   });
-
 
   // Test for login functionalities
   it("should login a user and return a token", async () => {
@@ -87,7 +81,6 @@ describe("Auth controller", (done) => {
     expect(response.body.token).toBeDefined();
   });
 
-
   // Test for user details
   it("should return 400 if email or password is missing", async () => {
     const response = await request(app)
@@ -97,7 +90,6 @@ describe("Auth controller", (done) => {
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("Please Provide email and password");
   });
-
 
   // Test for invalid email
   it("should return 400 if email does not exist", async () => {
@@ -135,58 +127,56 @@ describe("Auth controller", (done) => {
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("Invalid email or password");
   });
-
 });
 
-  // Test for profile routes
+// Test for profile routes
 
-  describe('Profile routes' , (done)=>{
-    let server;
+describe("Profile routes", (done) => {
+  let server;
 
-    beforeAll(()=>{
-      server = app.listen(4000)
-    });
-
-    afterAll(()=>{
-      server.close(done);
-    });
-
-    afterEach(()=>{
-      jest.clearAllMocks();
-    });
-
-    it('should return profile and authenticate token' , async()=>{
-      const user = {
-        id : 1,
-        username : "testuser",
-        email : "test@example.com",
-      };
-
-      authMiddleware.mockImplementation((req,res,next)=>{
-        req.user = user;
-        next();
-      });
-
-      User.findById.mockImplementation((id,callback)=>{
-        callback(null , [user]);
-      });
-
-      const token  = jwt.sign({id:user.id} , process.env.JWT_SECRET_KEY , {expiresIn : '1h'});
-
-      const response = await request(app)
-      .get('/api/profile')
-      .set('Authorization' , `Bearer ${token}`);
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({
-        id : user.id,
-        username : user.username,
-        email : user.email
-      });
-
-    });
-
-
-
+  beforeAll(() => {
+    server = app.listen(4000);
   });
 
+  afterAll(() => {
+    server.close(done);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return profile and authenticate token", async () => {
+    const user = {
+      id: 1,
+      username: "testuser",
+      email: "test@example.com",
+    };
+
+    authMiddleware.mockImplementation((req, res, next) => {
+      req.user = user;
+      next();
+    });
+
+    User.findById.mockImplementation((id, callback) => {
+      callback(null, [user]);
+    });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    const response = await request(app)
+      .get("/api/profile")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+  });
+});
+
+// working ......
